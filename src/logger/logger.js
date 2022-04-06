1f686cb5d5495007ea9e6c6782e805d378aaa059
tejasvi.fast <tejasvi.fast@gmail.com>
2022-04-06 21:31:48 +0530

    This is first problem commit

---
+++ src/logger/logger.js
@@ -0,0 +1,5 @@
+let log = function(){
+    console.log("Welcome to my application. I am <TEJASVI KUMAR> and a part of FunctionUp Thorium cohort."
+        );
+}
+module.exports.logging=log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
+const logger = require("../logger/logger")
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    console.log('Calling log function');
+    logger.logging()
     res.send('My first ever api!')
+
 });
 
+router.get('/test-me1', function (req, res) {
+    res.send('My second ever api!')
+});
 module.exports = router;
 // adding this comment for no reason
