497db5a95ab92981f5412172d10b57bdc77b983f
tejasvi.fast <tejasvi.fast@gmail.com>
2022-04-08 03:11:59 +0530

    assignment 2 completed of postman

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,7 +8,6 @@ router.get('/user-profile/:abcd', function(req, res) {
     console.log(req.params.abcd)
     res.send('dummy response')
 })
-
 router.get('/test-me', function (req, res) {
     console.log('------------------')
     console.log(req)
@@ -16,18 +15,59 @@ router.get('/test-me', function (req, res) {
     console.log('These are the request query parameters: ', req.query)
     res.send('My first ever api!')
 });
+//====================================================================================================
+//Problem 1
 const arrayMovie =["Dangal","rang de basanti","demon slayer","dr stone","full-metal alchemist"]
 router.get('/movies', function (req, res) {
-    // for(let i=0 ;i<arrayMovie.length;i++){
-                
-    // }
-    res.send(arrayMovie[4])
-    
+    res.send(arrayMovie)  
 });
-
-
-
-
+//=====================================================================================================
+//Problem 2 and 3
+router.get('/movies/:indexNumber', function (req, res) {
+    const arrayMovie1=["rang de basnasti", "the shining", "lord of the rings", "bartman begins"]
+    const index =req.params.indexNumber
+    if(index<=arrayMovie1.length-1)
+    {res.send(arrayMovie1[index]) }
+    else
+    {res.send("Please enter valid index value")} 
+});
+//========================================================================================================
+//Problem 4
+const arrayObjMovie =[ {
+    "id": 1,
+    "name": "rang de basnasti"
+   }, {
+    "id": 2,
+    "name": "shining"
+   }, {
+    "id": 3,
+    "name": "lord of the rings"
+   }, {
+    "id": 4,
+    "name": "bartman begins"
+   }]
+  
+  router.get('/films', function (req, res) {
+    res.send(arrayObjMovie)  
+});
+//===================================================================
+//Problem 5
+router.get('/films/:filmId', function(req, res) {
+    const filmIndex = req.params.filmId
+    for(let i=0;i<arrayObjMovie.length;i++)
+    {
+            if(arrayObjMovie[i].id==filmIndex)
+            {
+                result = arrayObjMovie[i]
+                break;
+            }
+            else
+            {
+            result = "No movie exists with this id"
+            }        
+    } 
+    res.send(result)
+})
 
 module.exports = router;
 // adding this comment for no reason
