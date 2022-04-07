b183481d87182cb76a7364fced9235146da2fa4a
tejasvi.fast <tejasvi.fast@gmail.com>
2022-04-07 19:34:21 +0530

    assignment completed array problem 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,13 +3,25 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+router.get('/candidates', function (req, res) {
+    const array =["a","b","c","d","e","f","g","h","i","j","k"]
+    let array1=[]
     console.log('------------------')
     console.log(req)
     console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+    console.log('These are the request query parameters: ', req.query.count)
+    if (req.query.count <=10)
+    {
+        for(let i=0;i<=req.query.count-1;i++)
+        {
+            array1[i]=array[i]
+        }
+    }
+    res.send(array1)
+    // console.log(array1)
+    // res.send('My first ever api!')
 });
+
 
 
 
