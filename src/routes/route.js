824aaaf0556568a4f1e4a0af7d6c5276d81b307d
tejasvi.fast <tejasvi.fast@gmail.com>
2022-04-07 23:12:19 +0530

    my first movie/

--- src/routes/route.js
+++ src/routes/route.js
@@ -16,6 +16,15 @@ router.get('/test-me', function (req, res) {
     console.log('These are the request query parameters: ', req.query)
     res.send('My first ever api!')
 });
+const arrayMovie =["Dangal","rang de basanti","demon slayer","dr stone","full-metal alchemist"]
+router.get('/movies', function (req, res) {
+    // for(let i=0 ;i<arrayMovie.length;i++){
+                
+    // }
+    res.send(arrayMovie[4])
+    
+});
+
 
 
 
