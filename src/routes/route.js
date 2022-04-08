9e53b1a76eed2ae62a6d0408a28794110c0d855f
tejasvi.fast <tejasvi.fast@gmail.com>
2022-04-08 17:58:37 +0530

    missing number 1st change

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,27 +3,33 @@ const logger = require('./logger')
 
 const router = express.Router();
 //=========================================================
-//Problem 1   Write a get api that returns a list of candidate names
-const array =["anuj","bijli","chetan","don","eel","flowery","govind","harry","itty","joy","kime"]
-router.get('/all-candidates', function (req, res) {
-    return res.send(array) 
+//Problem 1  [1,2,3,4,5,7,8,9]
+const arrayNum =[1,2,3,4,5,7,8,9]
+router.get('/missingNum', function (req, res) {
+    let n = arrayNum.length+1 ,sum =0 ,missingNum
+    const sumNnum = n*(n+1)/2
+    for(let i=0;i<arrayNum.length;i++){
+        sum = sum + arrayNum[i]
+    }
+    missingNum =  sumNnum -sum
+    res.send([missingNum]) 
 });
 //========================================================
-//Problem 2  Write a get api that returns a list of candidates with size equal to the count value recieved in the query param.
-//COnsider that the count value is greater than 0 and less or equal to 10.
-// The request you will send should have a url like this - http://localhost:3000/candidates?count=3
-router.get('/candidates', function (req, res) {
-    let array1=[]
-    // console.log(req)
-    // console.log('These are the request query parameters: ', req.query.count)
-    if (req.query.count <=10)
-    {
-        for(let i=0;i<req.query.count;i++)
-        {
-            array1[i]=array[i]
-        }
+//Problem 2 [33,34,35,37,38,39]
+router.get('/ap', function (req, res) {
+    let arrayNum1=[33,34,35,36,38,39,40,41,42,43]
+    let n = arrayNum1.length+1 ,sum1 =0
+    let a =arrayNum1[0] , l=arrayNum1[arrayNum1.length-1]
+    for(let i=0;i<arrayNum1.length;i++){
+        sum1 = sum1 + arrayNum1[i]
     }
-    return res.send(array1) 
+    let missingNum1 =  n/2*(a+l) -sum1
+    return res.send([missingNum1]) 
+});
+
+router.post('/ap', function (req, res) {
+    let arrayNum1=[33,34,35,36,38,39,40,41,42,43]
+    
 });
 
 
