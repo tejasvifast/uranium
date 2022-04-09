9ff592f35ff3eceba6ddf8a4486110ee0d81bab0
tejasvi.fast <tejasvi.fast@gmail.com>
2022-04-09 22:58:23 +0530

    player assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { status } = require('express/lib/response');
 
 const router = express.Router();
 
@@ -34,13 +35,10 @@ router.get('/test-api4', function (req, res) {
     res.send( { msg: "Hi FUnctionUp..again..this is another similar api ..not I am getting bored!"} )
 });
 
-
 router.get('/test-api5', function (req, res) {
 
     res.send( { msg: "Hi FUnctionUp" , name:"FunctionUp", age: "100"} )
 });
-
-
 
 router.get('/test-api6', function (req, res) {
 
@@ -51,8 +49,6 @@ router.post('/test-post1', function (req, res) {
 
     res.send( {  msg: "hi guys"  }   )
 });
-
-
 // to send data in  post request-> prefer sending in BODY -> click body-raw-json
 router.post('/test-post2', function (req, res) {
     let data= req.body
@@ -65,6 +61,62 @@ const randomController= require("../controllers/randomController.js")
 //write a post request to accept an element in post request body and add it to the given array and return the new array
 router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER
 
-
-
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": ["swimming"]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": ["soccer"],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": ["soccer"],
+       },
+   ]
+   router.post('/players', function (req, res) {
+       let result = true;
+    for(let i=0;i<players.length;i++)
+    {
+        if(players[i]["name"]==req.body["name"])
+        {
+         result = false
+        }
+    } 
+    if(result==true)
+    {
+        players.push(req.body)
+    }
+       res.send(  { data: players , status: result }  )
+   })
+//====================================================================
 module.exports = router;
+
+
+    // for(let i=0;i<players.length;i++)
+    // {
+    //     if (players[i]["name"]!=req.body["name"])
+    //     {
+    //         result = true
+    //     }
+    //     else if(players[i]["name"]==req.body["name"])
+    //     {
+    //         result =false
+    //         break;
+    //     }   
+    // } 
+    // if(result == true){
+    //     players.push(req.body)
+    // }
