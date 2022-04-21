1cee787eee5fae1cee6b46ff3012131152419c15
tejasvi.fast <tejasvi.fast@gmail.com>
2022-04-21 18:39:19 +0530

    maam assignment fie

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Functionup_user1:xjCaggwJRDuZ6bjP@my-first-cluster.sfq4n.mongodb.net/auth1", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
